f96c3471d5979079cd0c6d56914e57c02596a478
Engramar Bollas <engramar.bollas@gmail.com>
2020-11-16 22:38:40 +1100

    Update js_1_58-72.js

--- js_1_58-72.js
+++ js_1_58-72.js
@@ -70,19 +70,142 @@ function testGreaterThan(val) {
 testGreaterThan(10);
 
 #Comparison with the Greater Than Or Equal To Operator
+function testGreaterOrEqual(val) {
+  if (val >= 20) {  // Change this line
+    return "20 or Over";
+  }
+
+  if (val >= 10) {  // Change this line
+    return "10 or Over";
+  }
+
+  return "Less than 10";
+}
+
+testGreaterOrEqual(10);
 
 #Comparison with the Less Than Operator
+function testLessThan(val) {
+  if (val < 25) {  // Change this line
+    return "Under 25";
+  }
+
+  if (val < 55) {  // Change this line
+    return "Under 55";
+  }
+
+  return "55 or Over";
+}
+
+testLessThan(10);
+
 
 #Comparison with the Less Than Or Equal To Operator
+function testLessOrEqual(val) {
+  if (val <= 12) {  // Change this line
+    return "Smaller Than or Equal to 12";
+  }
+
+  if (val <= 24) {  // Change this line
+    return "Smaller Than or Equal to 24";
+  }
+
+  return "More Than 24";
+}
+
+testLessOrEqual(10);
+
 
 #Comparisons with the Logical And Operator
+function testLogicalAnd(val) {
+  // Only change code below this line
+
+  if (val <= 50 && val >=25 ) {  
+      return "Yes";
+  }
+
+  // Only change code above this line
+  return "No";
+}
+
+testLogicalAnd(10);
+
 
 #Comparisons with the Logical Or Operator
+function testLogicalOr(val) {
+  // Only change code below this line
+
+  if (val < 10 || val > 20 ) {
+    return "Outside";
+  }
+  // Only change code above this line
+  return "Inside";
+}
+
+testLogicalOr(15);
 
 #Introducing Else Statements
+function testElse(val) {
+  var result = "";
+  // Only change code below this line
+
+  if (val > 5) {
+    result = "Bigger than 5";
+  } else {
+    result = "5 or Smaller";
+  }
+
+  // Only change code above this line
+  return result;
+}
+
+testElse(4);
+
 
 #Introducing Else If Statements
+function testElseIf(val) {
+  if (val > 10) {
+    return "Greater than 10";
+  } else if (val < 5) {
+    return "Smaller than 5";
+  } else {
+    return "Between 5 and 10";
+  }
+}
+
+testElseIf(7);
+
 
 #Logical Order in If Else Statements
+function orderMyLogic(val) {
+  if (val < 5) {
+    return "Less than 5";
+  } else if (val < 10) {
+    return "Less than 10";
+  } else {
+    return "Greater than or equal to 10";
+  }
+}
+
+orderMyLogic(7);
+
 
 #Chaining If Else Statements
+function testSize(num) {
+  // Only change code below this line
+  if (num < 5) {
+    return 'Tiny'; 1    
+  } else if (num < 10) {
+    return 'Small'; 
+  } else if (num < 15) {
+    return 'Medium'; 
+  } else if (num < 20) {
+    return 'Large'; 
+  } else {
+    return 'Huge'; 
+  }
+
+  // Only change code above this line
+}
+
+testSize(7);
